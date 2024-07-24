8ef23a92ab970021f6840731252487b2c8779b87
black_mamba <vasilsemen04@gmail.com>
2024-07-24 21:49:37 +0300

    добавление модели Comment и многое другое в шаблонах

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog', 
+    'django_extensions', 
 ]
 
 MIDDLEWARE = [
@@ -117,7 +119,18 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR/'static'
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Конфигурация сервера электронной почты
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'vasilsemen04@gmail.com'
+EMAIL_HOST_PASSWORD = 'bbkh vlub sdqq euhy'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
